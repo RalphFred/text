87f1d67f40e32f0391e7fd072c3b6c067d210ac7
RalphFred <raphaelgbaorun@gmail.com>
2024-03-11 11:48:16 +0100

    fix: worked on the API key error

--- utils/index.ts
+++ utils/index.ts
@@ -6,8 +6,7 @@
       method: "POST",
       headers: {
         "content-type": "application/x-www-form-urlencoded",
-        'X-RapidAPI-Key': '3088144f5cmsh2ecbf9657015e88p145729jsnaa0931a6397e',
-        // "X-RapidAPI-Key": process.env.RAPIDAPI_KEY as string,
+        "X-RapidAPI-Key": process.env.RAPIDAPI_KEY as string,
         "X-RapidAPI-Host": "text-translator2.p.rapidapi.com",
       },
       body: new URLSearchParams({
